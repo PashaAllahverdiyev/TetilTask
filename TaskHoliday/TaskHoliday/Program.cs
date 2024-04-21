2d7fd578859b5c1201819bc7196d65696ede84ad
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-04-21 12:49:25 +0400

    Add files via upload

---
+++ TaskHoliday/TaskHoliday/Program.cs
@@ -0,0 +1,71 @@
+﻿internal class Program
+{
+    //Task1 Ters cevir
+    //static void Main(string[] arr)
+    //{
+    //    Ters();
+    //}
+    //static void Ters()
+    //{
+    //    string text ="Ali Sadat Mirqubad";
+    //    for (int i = text.Length - 1; i >= 0; i--)
+    //    {
+    //        Console.Write(text[i]);
+
+
+
+    //    }
+    //}
+    //Task2 Reqemi ters cevir
+    //static void Main(string[] args)
+    //{
+    //    Console.Write("eded yaz: ");
+    //    int num = Convert.ToInt32(Console.ReadLine());
+    //    int num2;
+    //    Console.Write("Ters yazilisi: ");
+    //    while (num > 0)
+    //    {
+    //        num2 = num % 10;
+    //        Console.Write(num2);
+    //        num /= 10;
+    //    }
+
+    //}
+    //Task3
+
+    
+    //Task4 Palindrom
+    //static void Main()
+    //{
+    //    Console.WriteLine("Soz Yaz");
+    //    string text = Console.ReadLine();
+
+    //    if (Palindrom(text))
+    //    {
+    //        Console.WriteLine($"{text} Palindromdur");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine($"{text} Palindrom deyil");
+
+    //    }
+    //}
+    //static bool Palindrom(string text)
+    //{
+    //    text = text.Replace(" "," ").ToLower();
+    //    int txtlength = text.Length;
+    //    int first = 0;
+    //    int end =txtlength - 1;
+    //    while (first < end)
+    //    {
+    //        if (text[first] != text[end])
+    //        {
+    //            return false;
+    //        }
+    //        first++;
+    //        end--;
+    //    }
+    //    return true;
+    //}
+
+}
